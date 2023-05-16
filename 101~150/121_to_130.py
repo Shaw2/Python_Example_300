13d4c3d5fa2178090eca399b4534e65eeb51d4bf
Shaw2 <49862639+Shaw2@users.noreply.github.com>
2023-05-17 01:06:20 +0900

    23.05.17

---
+++ 101~150/121_to_130.py
@@ -0,0 +1,21 @@
+# user_input = input('대소문자를 판별해드리죠. : ')
+# if user_input.islower():
+#     trans_input = user_input.upper()
+#     print(trans_input)
+# else:
+#     trans_input = user_input.lower()
+#     print(trans_input)
+
+# score_input = int(input('점수를 입력해주세요 : '))
+# if score_input <= 20:
+#     grade = 'E'
+# elif score_input <= 40:
+#     grade = 'D'
+# elif score_input <= 60:
+#     grade = 'C'
+# elif score_input <= 80:
+#     grade = 'B'
+# elif score_input <= 100:
+#     grade = 'A'
+# print('grade is {}'.format(grade))
+
