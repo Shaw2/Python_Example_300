67037439586d61bcaefe91e1f15c1d1ebfe68ed5
Shaw2 <49862639+Shaw2@users.noreply.github.com>
2023-05-28 00:13:22 +0900

    23.05.27

--- 101~150/131_to_140.py
+++ 101~150/131_to_140.py
@@ -1,1 +1,43 @@
 # 반복문 시작
+
+# 과일 = ["사과", "귤", "수박"]
+# for 변수 in 과일:
+#     print(변수)
+
+# 과일 = ["사과", "귤", "수박"]
+# for 변수 in 과일:
+#   print("#####")
+
+# A = ['A', 'B', 'C']
+# for 변수 in A:
+#     print(변수)
+
+# for 변수 in ["A", "B", "C"]:
+#     print("출력:", 변수)
+            
+# for 변수 in ["A", "B", "C"]:
+#       b = 변수.lower()
+#   print("변환:", b)
+      
+# numbers = [10, 20, 30]
+# for number in numbers:
+#     print(number)
+    
+# print(10)
+# print(20)
+# print(30)
+
+# numbers = [10, 20, 30]
+# for number in numbers:
+#     print(number)
+#     print('-'*6)
+
+# numbers = [10, 20, 30]
+# print('++++')
+# for number in numbers:
+#     print(number)
+
+# numbers = [10, 20, 30, 40]
+# for count in numbers:
+#     print('-'*6)
+
---
+++ 101~150/151_to_160.py
@@ -0,0 +1,53 @@
+# 리스트 = [3, -20, -3, 44]
+# for number in 리스트:
+#     if number < 0 :
+#         print(number)
+
+# 리스트 = [3, 100, 23, 44]
+# for number in 리스트:
+#     if (number%3 == 0):
+#         print(number) 
+
+# 리스트 = [13, 21, 12, 14, 30, 18]
+# for number in 리스트:
+#     if number < 20:  # and로 찍어도 괜춘
+#         if number%3 ==0:
+#             print(number)
+            
+# 리스트 = ["I", "study", "python", "language", "!"]
+# for text in 리스트:
+#     if len(text)>=3:
+#         print(text)
+
+# 리스트 = ["A", "b", "c", "D"]
+# for text in 리스트:
+#     if text.isupper():
+#         print(text)
+
+# 리스트 = ["A", "b", "c", "D"]
+# for text in 리스트:
+# #    if text.islower():
+#     if text.isupper() == False:
+#         print(text)
+
+# 리스트 = ['dog', 'cat', 'parrot']
+# for text in 리스트:
+#     Capital = text[0].upper()
+#     print(Capital+text[1:])
+
+# 리스트 = ['hello.py', 'ex01.py', 'intro.hwp']
+# for file_name in 리스트:
+#     go = file_name.split('.')[0]
+#     print(go)
+
+# 리스트 = ['intra.h', 'intra.c', 'define.h', 'run.py']
+# for file_name in 리스트:
+#     end = file_name.split('.')[1]
+#     if end == 'h':
+#         print(file_name)
+
+# 리스트 = ['intra.h', 'intra.c', 'define.h', 'run.py']
+# for file_name in 리스트:
+#     end = file_name.split('.')[1]
+#     if end == 'h' or end == 'c':
+#         print(file_name)
